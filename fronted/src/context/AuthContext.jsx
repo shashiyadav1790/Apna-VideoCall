06a6cd17e4497083b2265cdcf48b0dbb9d561f17
shashi yadav <shashiyadav1790@gmail.com>
2025-01-03 15:15:42 +0530

    some changes

--- fronted/src/context/AuthContext.jsx
+++ fronted/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import server from "../eviroment";
+import server from "../enviroment";
 
 export const AuthContext = createContext({});
 
